8269a87d6e002b6b39fbdbf60058f992fef0af5b
sharunbansal03 <sharun.bansal03@gmail.com>
2023-02-18 02:07:19 +0530

    reverted changes

--- src/main/java/GenericUtilities/ListenerImplementationClass.java
+++ src/main/java/GenericUtilities/ListenerImplementationClass.java
@@ -1,8 +1,6 @@
 package GenericUtilities;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.testng.ITestContext;
 import org.testng.ITestListener;
@@ -18,33 +16,33 @@ import com.aventstack.extentreports.Status;
  *
  */
 public class ListenerImplementationClass extends ExtentManagerUtility implements ITestListener {
-	static Map<Integer, ExtentTest> extentTestMap = new HashMap<>();
-	
+
+	ExtentTest test;
+	private static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<ExtentTest>();
 
 	public void onTestStart(ITestResult result) {
 		System.out.println("inside on test start");
 		String methodName = result.getMethod().getMethodName();
-		ExtentTest test = report.createTest(methodName);
-		extentTestMap.put((int) Thread.currentThread().getId(), test);
-		
-		extentTestMap.get((int) Thread.currentThread().getId()).log(Status.INFO, "Test Execution Started: " + methodName);
+		test = report.createTest(methodName);
+		extentTest.set(test);
+		extentTest.get().log(Status.INFO, "Test Execution Started: " + methodName);
 	}
 
 	public void onTestSuccess(ITestResult result) {
-		extentTestMap.get((int) Thread.currentThread().getId()).log(Status.PASS, "Test passed: " + result.getMethod().getMethodName());
+		extentTest.get().log(Status.PASS, "Test passed: " + result.getMethod().getMethodName());
 	}
 
 	public void onTestFailure(ITestResult result) {
 		JavaUtility jUtils = new JavaUtility();
 		String methodName = result.getMethod().getMethodName();
-		extentTestMap.get((int) Thread.currentThread().getId()).log(Status.FAIL, "Test Script failed - " + methodName);
-		extentTestMap.get((int) Thread.currentThread().getId()).log(Status.FAIL, result.getThrowable());
+		extentTest.get().log(Status.FAIL, "Test Script failed - " + methodName);
+		extentTest.get().log(Status.FAIL, result.getThrowable());
 
 		WebDriverUtility wUtils = new WebDriverUtility();
 		try {
 			String path = wUtils.takeScreenshot(BaseClass.sDriver,
 					methodName + "_" + jUtils.getSystemDataAndTimeInFormat());
-			extentTestMap.get((int) Thread.currentThread().getId()).addScreenCaptureFromPath(path);
+			extentTest.get().addScreenCaptureFromPath(path);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -54,12 +52,12 @@ public class ListenerImplementationClass extends ExtentManagerUtility implements
 	public void onTestSkipped(ITestResult result) {
 		JavaUtility jUtils = new JavaUtility();
 		String methodName = result.getMethod().getMethodName();
-		extentTestMap.get((int) Thread.currentThread().getId()).log(Status.SKIP, methodName);
-		extentTestMap.get((int) Thread.currentThread().getId()).log(Status.SKIP, result.getThrowable());
+		extentTest.get().log(Status.SKIP, methodName);
+		extentTest.get().log(Status.SKIP, result.getThrowable());
 		try {
 			String path = new WebDriverUtility().takeScreenshot(BaseClass.sDriver,
 					methodName + "_" + jUtils.getSystemDataAndTimeInFormat());
-			extentTestMap.get((int) Thread.currentThread().getId()).addScreenCaptureFromPath(path);
+			extentTest.get().addScreenCaptureFromPath(path);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
